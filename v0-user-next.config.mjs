68e93ca9a0ee52ae577cb653b31f3bd129bb4a94
xiang <xiang@mac.mynetworksettings.com>
2025-04-14 02:02:50 -0400

    补充视频文件

--- v0-user-next.config.mjs
+++ v0-user-next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // 启用静态导出
+  //output: 'export',  // 启用静态导出
   images: {
     unoptimized: true,  // 对于静态导出，需要禁用图片优化
   },
